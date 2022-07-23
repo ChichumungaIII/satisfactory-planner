1d82d31be82171494f108cebe1a62a9db264c64a
chichumungaiii <chris@thesharpehouse.com>
2022-07-23 11:58:45 -0700

    Adds a Rational number class for increased numerical precision during mathematically heavy operations.

---
+++ src/main/kotlin/util/math/Rational.kt
@@ -0,0 +1,91 @@
+package util.math
+
+import kotlin.math.abs
+
+/**
+ * A Rational number is a pair of [Long] values (n, d) representing the ratio of the two, n / d.
+ *
+ * @property n The numerator of the Rational number.
+ * @property d The denominator of the Rational number, which must be positive.
+ * @constructor
+ */
+class Rational private constructor(
+    private val n: Long,
+    private val d: Long,
+) {
+    companion object {
+        /**
+         * Creates a new [Rational] number, n / d.
+         *
+         * @param n The numerator of the Rational number.
+         * @param d The denominator of the Rational number.
+         * @throws IllegalStateException if `d` is zero.
+         */
+        fun create(n: Long, d: Long = 1) =
+            if (d < 0) Rational(-n, -d) else Rational(n, d)
+
+        private fun gcd(a: Long, b: Long): Long {
+            val pA = abs(a)
+            val pB = abs(b)
+            return if (pA > pB) gcdImpl(pB, pA) else gcdImpl(pA, pB)
+        }
+
+        private fun gcdImpl(a: Long, b: Long): Long =
+            if (a == 0L) b else gcdImpl(b % a, a)
+    }
+
+    init {
+        check(d > 0) { "Rational number [$this] requires a positive denominator." }
+    }
+
+    /** @property norm The reduced form of this Rational number. */
+    private val norm: Rational by lazy {
+        val gcd = gcd(n, d)
+        if (gcd == 1L) this else Rational(n / gcd, d / gcd)
+    }
+
+    /**
+     * Returns this Rational number in reduced form.
+     *
+     * If the number is already reduced, returns this Rational number.
+     */
+    fun norm() = norm
+
+    operator fun unaryPlus() = this
+    operator fun unaryMinus() = Rational(-n, d)
+
+    operator fun plus(other: Rational) =
+        Rational(
+            n * other.d + other.n * d,
+            d * other.d
+        )
+
+    operator fun minus(other: Rational) =
+        Rational(
+            n * other.d - other.n * d,
+            d * other.d
+        )
+
+    operator fun times(other: Rational) =
+        Rational(
+            n * other.n,
+            d * other.d
+        )
+
+    operator fun div(other: Rational) =
+        create(
+            n * other.d,
+            other.n * d
+        )
+
+    operator fun compareTo(other: Rational) =
+        (n * other.d).compareTo(other.n * d)
+
+    /** Returns true if [other] is a [Rational] number numerically equal to this. */
+    override fun equals(other: Any?) =
+        other is Rational && this.norm.n == other.norm.n && this.norm.d == other.norm.d
+
+    override fun hashCode() = (norm.n * 31 + norm.d).toInt()
+
+    override fun toString() = if (d == 1L) "$n" else "$n/$d"
+}
---
+++ src/test/kotlin/util/math/RationalTest.kt
@@ -0,0 +1,90 @@
+package util.math
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertSame
+import kotlin.test.assertTrue
+
+class RationalTest {
+    @Test
+    fun create_normalizesSign() {
+        assertEquals(Rational.create(-2, 3), Rational.create(2, -3))
+    }
+
+    @Test
+    fun create_zeroDenominator_throws() {
+        val expected = assertFailsWith<IllegalStateException> { Rational.create(5, 0) }
+        assertEquals("Rational number [5/0] requires a positive denominator.", expected.message)
+    }
+
+    @Test
+    fun norm_returnsReducedValue() {
+        assertEquals("3/4", Rational.create(9, 12).norm().toString())
+    }
+
+    @Test
+    fun norm_alreadyReduced_returnsSameInstance() {
+        val value = Rational.create(7, 13)
+        assertSame(value, value.norm())
+    }
+
+    @Test
+    fun unaryPlus_returnsSameInstance() {
+        val value = Rational.create(4, 8)
+        assertSame(value, +value)
+    }
+
+    @Test
+    fun unaryMinus_returnsNegatedValue() {
+        val value = Rational.create(1, 2)
+        assertEquals(Rational.create(-1, 2), -value)
+    }
+
+    @Test
+    fun plus_returnsSum() {
+        assertEquals(
+            Rational.create(7, 12),
+            Rational.create(1, 3) + Rational.create(1, 4)
+        )
+    }
+
+    @Test
+    fun minus_returnsDifference() {
+        assertEquals(
+            Rational.create(1, 3),
+            Rational.create(7, 12) - Rational.create(1, 4)
+        )
+    }
+
+    @Test
+    fun times_returnsProduct() {
+        assertEquals(
+            Rational.create(2, 5),
+            Rational.create(3, 5) * Rational.create(2, 3)
+        )
+    }
+
+    @Test
+    fun div_returnsQuotient() {
+        assertEquals(
+            Rational.create(3, 5),
+            Rational.create(2, 5) / Rational.create(2, 3)
+        )
+    }
+
+    @Test
+    fun compareTo_returnsLessThan() {
+        assertTrue { Rational.create(1, 3) < Rational.create(1, 2) }
+    }
+
+    @Test
+    fun compareTo_returnsEquals() {
+        assertEquals(0, Rational.create(1, 2).compareTo(Rational.create(2, 4)))
+    }
+
+    @Test
+    fun compareTo_returnsGreaterThan() {
+        assertTrue { Rational.create(1, 2) > Rational.create(1, 3) }
+    }
+}
