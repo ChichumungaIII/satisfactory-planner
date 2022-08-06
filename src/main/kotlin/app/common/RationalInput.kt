7cc2d9f124e5d9a350aea595387a48cabfc7f862
chichumungaiii <chris@thesharpehouse.com>
2022-08-06 13:15:08 -0700

    Implements a RationalInput component to support Rational number entry.

--- src/main/kotlin/app/common/RationalInput.kt
+++ src/main/kotlin/app/common/RationalInput.kt
@@ -1,7 +1,13 @@
 package app.common
 
+import csstype.px
+import mui.material.FormControlVariant
+import mui.material.TextField
+import mui.system.sx
 import react.FC
 import react.Props
+import react.ReactNode
+import react.dom.onChange
 import react.useState
 import util.math.Rational
 
@@ -12,5 +18,26 @@ external interface RationalInputProps : Props {
 
 val RationalInput = FC<RationalInputProps>("RationalInput") { props ->
     var text by useState(props.value?.toString() ?: "")
+    var error by useState(false)
 
+    TextField {
+        sx {
+            if (!error) marginBottom = (22.91).px
+        }
+
+        variant = FormControlVariant.outlined
+        this.error = error
+        if (error) helperText = ReactNode("Value must be a rational ")
+
+        value = text
+        onChange = { event ->
+            val nextText = event.target.asDynamic().value as String
+            text = nextText
+
+            val next = Rational.parse(nextText)
+            props.setValue(next)
+
+            error = nextText.isNotEmpty() && next == null
+        }
+    }
 }
