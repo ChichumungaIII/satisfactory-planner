edbf68ff9e702f1097bc164a8bdc29b0ce014d3d
chichumungaiii <chris@thesharpehouse.com>
2022-09-17 11:15:23 -0700

    Use the generic type `V` consistently across Expression and Constraint.

--- src/commonMain/kotlin/util/math/Expression.kt
+++ src/commonMain/kotlin/util/math/Expression.kt
@@ -1,7 +1,7 @@
 package util.math
 
-class Expression<T, N : Numeric<N>> private constructor(
-    private val coefficients: Map<T, N>,
+class Expression<V, N : Numeric<N>> private constructor(
+    private val coefficients: Map<V, N>,
     private val numbers: Numeric.Factory<N>,
 ) {
     companion object {
@@ -9,17 +9,17 @@ class Expression<T, N : Numeric<N>> private constructor(
     }
 
     val terms = coefficients.keys
-    fun get(term: T) = coefficients[term] ?: numbers.zilch()
+    fun get(term: V) = coefficients[term] ?: numbers.zilch()
 
     operator fun unaryPlus() = this
     operator fun unaryMinus() = Expression(coefficients.mapValues { (_, n) -> -n }.toMap(), numbers)
 
-    operator fun plus(other: Expression<T, N>) = join(other, Numeric<N>::plus)
-    operator fun minus(other: Expression<T, N>) = join(other, Numeric<N>::minus)
-    private fun join(other: Expression<T, N>, merge: (N, N) -> N) =
+    operator fun plus(other: Expression<V, N>) = join(other, Numeric<N>::plus)
+    operator fun minus(other: Expression<V, N>) = join(other, Numeric<N>::minus)
+    private fun join(other: Expression<V, N>, merge: (N, N) -> N) =
         Expression((terms + other.terms).associateWith { term -> merge(get(term), other.get(term)) }, numbers)
 
-    operator fun invoke(values: Map<T, N>) =
+    operator fun invoke(values: Map<V, N>) =
         coefficients.map { (term, c) -> c * (values[term] ?: numbers.zilch()) }.reduce(Numeric<N>::plus)
 
     override fun equals(other: Any?) = other is Expression<*, *> && coefficients == other.coefficients
