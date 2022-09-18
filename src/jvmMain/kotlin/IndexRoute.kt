056f575d05e0639ce590150919350b816e0f8efc
chichumungaiii <chris@thesharpehouse.com>
2022-09-18 10:47:08 -0700

    Organize the server routes into separate files.

---
+++ src/jvmMain/kotlin/IndexRoute.kt
@@ -0,0 +1,35 @@
+package com.chichumunga.satisfactory
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.call
+import io.ktor.server.html.respondHtml
+import io.ktor.server.routing.Routing
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
+import kotlinx.html.body
+import kotlinx.html.head
+import kotlinx.html.link
+import kotlinx.html.meta
+import kotlinx.html.script
+import kotlinx.html.title
+
+fun Routing.indexRoute() {
+    route("/{...}") {
+        get {
+            call.respondHtml(HttpStatusCode.OK) {
+                head {
+                    meta { charset = "UTF-8" }
+                    title("Satisfactory Planner")
+
+                    link {
+                        rel = "stylesheet"
+                        href = "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    }
+                }
+                body {
+                    script { src = "/static/satisfactory-planner.js" }
+                }
+            }
+        }
+    }
+}
---
+++ src/jvmMain/kotlin/StaticRoute.kt
@@ -0,0 +1,9 @@
+package com.chichumunga.satisfactory
+
+import io.ktor.server.http.content.resources
+import io.ktor.server.http.content.static
+import io.ktor.server.routing.Routing
+
+fun Routing.staticRoute() {
+    static("/static") { resources() }
+}
