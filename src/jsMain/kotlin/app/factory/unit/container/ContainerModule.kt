1d1c0aaa2da0eedc7286af14c7c06fd31354e248
chichumungaiii <chris@thesharpehouse.com>
2022-11-23 19:30:06 -0800

    Invert the FactoryUnit components so that they represent modules within FactoryUnitContainer rather than being composed of it.

---
+++ src/jsMain/kotlin/app/factory/unit/container/ContainerModule.kt
@@ -0,0 +1,21 @@
+package app.factory.unit.container
+
+import app.factory.model.FactoryUnit
+import app.factory.model.FactoryUnitContainer
+import app.factory.selectunit.SelectUnitSpeedDial
+import app.factory.unit.factoryUnit
+import react.FC
+import react.Props
+
+external interface ContainerModuleProps : Props {
+    var container: FactoryUnitContainer
+    var setUnit: (FactoryUnit) -> Unit
+}
+
+val ContainerModule = FC<ContainerModuleProps>("ContainerModule") { props ->
+    props.container.units.forEachIndexed { i: Int, unit: FactoryUnit ->
+        factoryUnit(unit) { props.setUnit(props.container.setUnit(i, it)) }
+    }
+
+    SelectUnitSpeedDial { onSelect = { props.setUnit(props.container.addUnit(it)) } }
+}
--- src/jsMain/kotlin/app/factory/unit/factoryUnit.kt
+++ src/jsMain/kotlin/app/factory/unit/factoryUnit.kt
@@ -4,23 +4,31 @@ import app.factory.model.FactoryUnit
 import app.factory.model.FactoryUnitArray
 import app.factory.model.FactoryUnitContainer
 import app.factory.model.ProductionBuilding
+import app.factory.unit.array.ArrayModule
+import app.factory.unit.container.ContainerModule
+import app.factory.unit.production.ProductionBuildingModule
 import react.ChildrenBuilder
 
 fun ChildrenBuilder.factoryUnit(unit: FactoryUnit, setUnit: (FactoryUnit) -> Unit) {
-    when (unit) {
-        is ProductionBuilding -> ProductionBuildingComponent {
-            production = unit
-            this.setUnit = { setUnit(it) }
-        }
+    FactoryUnitComponent {
+        this.unit = unit
+        this.setUnit = setUnit
 
-        is FactoryUnitContainer -> ContainerComponent {
-            container = unit
-            this.setUnit = { setUnit(it) }
-        }
+        when (unit) {
+            is ProductionBuilding -> ProductionBuildingModule {
+                this.production = unit
+                this.setUnit = setUnit
+            }
 
-        is FactoryUnitArray -> ArrayComponent {
-            array = unit
-            this.setUnit = { setUnit(it) }
+            is FactoryUnitContainer -> ContainerModule {
+                this.container = unit
+                this.setUnit = setUnit
+            }
+
+            is FactoryUnitArray -> ArrayModule {
+                this.array = unit
+                this.setUnit = setUnit
+            }
         }
     }
 }
