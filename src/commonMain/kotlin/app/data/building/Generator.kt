406749b8436941973e8e7955317554caf5c6374b
chichumungaiii <chris@thesharpehouse.com>
2022-12-12 17:06:57 -0800

    Fork Building into `Generator` and `ProductionBuilding`.

---
+++ src/commonMain/kotlin/app/data/building/Generator.kt
@@ -0,0 +1,35 @@
+package app.data.building
+
+import app.data.Recipe
+import util.math.Rational
+import util.math.q
+
+enum class Generator(
+    override val displayName: String,
+    override val generation: Rational,
+    override val recipes: List<Recipe>,
+) : Building {
+    BIOMASS_BURNER(
+        "Biomass Burner",
+        generation = 30.q,
+        recipes = listOf(
+            // TODO: Add biofuel recipes
+        ),
+    ),
+    COAL_GENERATOR(
+        "Coal Generator",
+        generation = 75.q,
+        recipes = listOf(
+            Recipe.BURN_COAL,
+        ),
+    ),
+    FUEL_GENERATOR(
+        "Fuel Generator",
+        generation = 150.q,
+        recipes = listOf(
+            Recipe.BURN_FUEL,
+        ),
+    );
+
+    override val consumption = 0.q;
+}
