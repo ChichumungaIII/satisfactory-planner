c16b728a9218e9daf02f2cb2c1202c61c2175d79
chichumungaiii <chris@thesharpehouse.com>
2023-03-10 09:55:33 -0800

    Add in extraction recipes so they can be used in factory plans.

--- src/commonMain/kotlin/app/data/building/Extractor.kt
+++ src/commonMain/kotlin/app/data/building/Extractor.kt
@@ -1,40 +1,34 @@
 package app.data.building
 
-import app.data.recipe.Recipe
+import app.data.recipe.ExtractionRecipe
 import util.math.Rational
 import util.math.q
 
 enum class Extractor(
     override val displayName: String,
     override val consumption: Rational,
-    override val recipes: List<Recipe>,
+    val rate: Rational,
 ) : ProductionBuilding {
     MINER_MK_1(
         "Miner Mk. 1",
         consumption = 5.q,
-        recipes = listOf(
-            // TODO: Add extractor recipes
-        ),
+        rate = 60.q,
     ),
     WATER_EXTRACTOR(
         "Water Extractor",
         consumption = 20.q,
-        recipes = listOf(
-            // TODO: Add extractor recipes
-        ),
+        rate = 120.q
     ),
     MINER_MK_2(
         "Miner Mk. 2",
         consumption = 12.q,
-        recipes = listOf(
-            // TODO: Add extractor recipes
-        ),
+        rate = 120.q,
     ),
     OIL_EXTRACTOR(
         "Oil Extractor",
         consumption = 40.q,
-        recipes = listOf(
-            // TODO: Add extractor recipes
-        ),
+        rate = 120.q,
     );
+
+    override val recipes by lazy { ExtractionRecipe.values().filter { it.extractor == this } }
 }
