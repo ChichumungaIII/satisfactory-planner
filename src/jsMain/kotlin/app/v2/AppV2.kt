12e32767e26dc7e0379d64f987dc157ae3932d3c
chichumungaiii <chris@thesharpehouse.com>
2023-03-18 13:37:20 -0700

    Refactor AppV2 to express the nested context structure more clearly.

--- src/jsMain/kotlin/app/v2/AppV2.kt
+++ src/jsMain/kotlin/app/v2/AppV2.kt
@@ -11,30 +11,29 @@ import app.v2.frame.title.TitleContextComponent
 import kotlinx.coroutines.MainScope
 import react.FC
 import react.Props
+import react.PropsWithChildren
+import react.ReactElement
+import react.create
 
 external interface AppV2Props : Props
 
 val AppScope = MainScope()
 
 val AppV2 = FC<AppV2Props>("AppV2") { _ ->
-    // Global theme
-    ThemeModule {
+    +listOf(
+        // Global theme
+        ThemeModule,
         // Global data
-        FactoryServiceContextProvider {
-            FactoryStoreContextProvider {
-                // Frame data
-                TitleContextComponent {
-                    FrameMenuOptionsContextComponent {
-                        // Specific data
-                        FactoriesContextProvider {
-                            FactoryContextProvider {
-                                // App
-                                FrameComponent {}
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+        FactoryServiceContextProvider,
+        FactoryStoreContextProvider,
+        // Frame data
+        TitleContextComponent,
+        FrameMenuOptionsContextComponent,
+        // Specific data
+        FactoriesContextProvider,
+        FactoryContextProvider,
+    ).nest(FrameComponent.create {})
 }
+
+private fun List<FC<PropsWithChildren>>.nest(element: ReactElement<*>): ReactElement<*> =
+    foldRight(element) { parent, child -> parent.create { +child } }
