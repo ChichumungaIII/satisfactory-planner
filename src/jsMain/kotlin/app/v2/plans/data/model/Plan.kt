5f5c1931667225bc3a1af8ba75a4600baac4f8a9
chichumungaiii <chris@thesharpehouse.com>
2023-04-21 12:29:49 -0700

    Update Plan modelling for the new UI.

--- src/jsMain/kotlin/app/v2/plans/data/model/Plan.kt
+++ src/jsMain/kotlin/app/v2/plans/data/model/Plan.kt
@@ -1,5 +1,6 @@
 package app.v2.plans.data.model
 
+import app.api.OptimizeRequest
 import app.data.Item
 import app.data.recipe.Recipe
 import app.data.recipe.componentRate
@@ -38,15 +39,48 @@ data class Plan(
 data class PlanInput(
   val item: Item? = null,
   val amount: Rational = 0.q,
+  val details: Boolean = false,
 )
 
 @Serializable
 data class PlanProduct(
   val item: Item? = null,
-  val exact: Boolean = false,
-  val amount: Rational = 0.q,
-  val maximum: Rational? = null,
-)
+  val requirement: Requirement = AtLeast(0.q),
+  val details: Boolean = false,
+) {
+  @Serializable
+  sealed interface Requirement {
+    fun toAtLeast(): AtLeast
+    fun toExactly(): Exactly
+    fun toBetween(): Between
+
+    fun toOutcome(item: Item): OptimizeRequest.Outcome
+  }
+
+  data class AtLeast(val minimum: Rational) : Requirement {
+    override fun toAtLeast() = this
+    override fun toExactly() = Exactly(minimum)
+    override fun toBetween() = Between(minimum, minimum)
+
+    override fun toOutcome(item: Item) = OptimizeRequest.Minimum(item, minimum)
+  }
+
+  data class Exactly(val amount: Rational) : Requirement {
+    override fun toAtLeast() = AtLeast(amount)
+    override fun toExactly() = this
+    override fun toBetween() = Between(amount, amount)
+
+    override fun toOutcome(item: Item) = OptimizeRequest.Exact(item, amount)
+  }
+
+  data class Between(val minimum: Rational, val maximum: Rational) : Requirement {
+    override fun toAtLeast() = AtLeast(minimum)
+    override fun toExactly() = Exactly(minimum)
+    override fun toBetween() = this
+
+    override fun toOutcome(item: Item) = OptimizeRequest.Range(item, minimum, maximum)
+  }
+}
 
 @Serializable
 data class PlanResult(
