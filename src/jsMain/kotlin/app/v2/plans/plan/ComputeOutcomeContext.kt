bbdc5550424713d54e209e377e3524026378fb4c
chichumungaiii <chris@thesharpehouse.com>
2023-04-23 10:59:54 -0700

    Reduce request noise and only update the plan if the response is up-to-date with the state of the UI.

--- src/jsMain/kotlin/app/v2/plans/plan/ComputeOutcomeContext.kt
+++ src/jsMain/kotlin/app/v2/plans/plan/ComputeOutcomeContext.kt
@@ -10,6 +10,7 @@ import app.v2.plans.data.model.Plan
 import app.v2.plans.data.model.PlanInput
 import app.v2.plans.data.model.PlanProduct
 import app.v2.plans.data.model.PlanResult
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import react.FC
 import react.PropsWithChildren
@@ -17,26 +18,42 @@ import react.ReducerInstance
 import react.createContext
 import react.useContext
 import react.useReducer
+import kotlin.time.Duration.Companion.milliseconds
 
 sealed interface ComputeOutcomeContextAction
 data class ForPlan(val plan: Plan) : ComputeOutcomeContextAction
 
-val ComputeOutcomeContext = createContext<ReducerInstance<OptimizeRequest?, ComputeOutcomeContextAction>>()
+val ComputeOutcomeContext = createContext<ReducerInstance<Unit, ComputeOutcomeContextAction>>()
+
+private val DEBOUNCE = 500.milliseconds
+
+private var current: Plan? = null
+private var latest: OptimizeRequest? = null
 
 val ComputeOutcomeContextComponent = FC<PropsWithChildren>("CreateOutcomeContextComponent") {
   val (_, updatePlan) = useContext(PlanContext)
 
-  val outcome = useReducer<OptimizeRequest?, ComputeOutcomeContextAction>({ current, action ->
+  val outcome = useReducer<Unit, ComputeOutcomeContextAction>({ _, action ->
     when (action) {
       is ForPlan -> {
-        val request = OptimizeRequest(
-          recipes = ProductionRecipe.values().toSet(),
-          inputs = action.plan.inputs.mapNotNull(PlanInput::toInput),
-          outcomes = action.plan.products.mapNotNull(PlanProduct::toOutcome),
-        )
+        val plan = action.plan
+        current = plan
 
         AppScope.launch {
+          delay(DEBOUNCE)
+          if (plan != current) return@launch
+
+          val request = OptimizeRequest(
+            recipes = ProductionRecipe.values().toSet(),
+            inputs = plan.inputs.mapNotNull(PlanInput::toInput),
+            outcomes = plan.products.mapNotNull(PlanProduct::toOutcome),
+          )
+          if (request == latest) return@launch
+          latest = request
+
           val response = optimize(request)
+          if (plan != current) return@launch
+
           val results = ProductionRecipe.values()
             .associateWith { response.outcome[it] }
             .mapNotNull { (recipe, rate) -> rate?.let { recipe to it } }
@@ -50,11 +67,9 @@ val ComputeOutcomeContextComponent = FC<PropsWithChildren>("CreateOutcomeContext
           )
           updatePlan(SavePlan(computedPlan))
         }
-
-        request
       }
     }
-  }, initialState = null)
+  }, initialState = Unit)
 
   ComputeOutcomeContext(outcome) {
     +it.children
