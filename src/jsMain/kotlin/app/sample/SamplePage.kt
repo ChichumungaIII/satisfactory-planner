37911acfcd9f455df29f5229829b1bc50a7df30e
chichumungaiii <chris@thesharpehouse.com>
2023-06-09 13:26:05 -0700

    Add a dialog to allow dynamic theme changes.

--- src/jsMain/kotlin/app/sample/SamplePage.kt
+++ src/jsMain/kotlin/app/sample/SamplePage.kt
@@ -3,40 +3,30 @@ package app.sample
 import app.themes.SampleTheme
 import app.themes.ThemeContext
 import csstype.None
-import csstype.pct
-import csstype.px
-import mui.icons.material.AccountTree
-import mui.icons.material.Add
-import mui.icons.material.ExpandMore
-import mui.icons.material.Factory
+import csstype.number
 import mui.icons.material.MenuOpen
+import mui.icons.material.Settings
 import mui.material.AppBar
 import mui.material.AppBarPosition
 import mui.material.Box
-import mui.material.Collapse
-import mui.material.Drawer
-import mui.material.DrawerVariant
 import mui.material.IconButton
-import mui.material.ListItem
-import mui.material.ListItemButton
-import mui.material.ListItemIcon
-import mui.material.ListItemText
-import mui.material.ListSubheader
 import mui.material.Toolbar
 import mui.material.Typography
 import mui.material.styles.TypographyVariant
 import mui.system.sx
 import react.FC
 import react.Props
-import react.create
 import react.useContext
 import react.useLayoutEffectOnce
+import react.useState
 
 external interface SamplePageProps : Props
 
 val SamplePage = FC<SamplePageProps>("SamplePage") {
   var theme by useContext(ThemeContext)
   useLayoutEffectOnce { theme = SampleTheme }
+
+  var settings by useState(false)
 
   AppBar {
     sx { backgroundImage = None.none }
@@ -52,8 +42,20 @@ val SamplePage = FC<SamplePageProps>("SamplePage") {
         variant = TypographyVariant.h1
         +"Satisfactory Planner"
       }
+
+      Box { sx { flexGrow = number(1.0) } }
+
+      IconButton {
+        Settings {}
+        onClick = { settings = true }
+      }
     }
   }
 
   SampleDrawer {}
+
+  SampleSettingsDialog {
+    open = settings
+    setOpen = { next -> settings = next }
+  }
 }
