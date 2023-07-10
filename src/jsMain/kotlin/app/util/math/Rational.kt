c174dc729bfc207552610c367e4a541fb8fd9a00
chichumungaiii <chris@thesharpehouse.com>
2023-07-09 17:24:48 -0700

    Delete an unused helper.

--- src/jsMain/kotlin/app/util/math/Rational.kt
+++
@@ -1,6 +0,0 @@
-package app.util.math
-
-import util.math.Rational
-
-fun Double.toFixed(places: Int) = asDynamic().toFixed(places) as String
-fun Rational.toFixed(places: Int) = toDouble().toFixed(places)
