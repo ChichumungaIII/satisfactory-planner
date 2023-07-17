6a137c6a29963cb4c2b9b74866372c94cb8fdbf5
chichumungaiii <chris@thesharpehouse.com>
2023-07-16 19:24:18 -0700

    Move DisplayNameStep content into its component.

--- src/jsMain/kotlin/app/routes/createsave/CreateSavePage.kt
+++ src/jsMain/kotlin/app/routes/createsave/CreateSavePage.kt
@@ -1,41 +1,26 @@
 package app.routes.createsave
 
-import app.api.save.v1.CreateSaveRequest
 import app.api.save.v1.Save
 import app.api.save.v1.SaveName
-import app.api.save.v1.SaveServiceJs
-import app.data.save.SavesListService
-import app.theme.AppThemeContext
-import app.util.launchMain
-import kotlinx.coroutines.delay
-import mui.lab.LoadingButton
 import mui.material.Orientation
-import mui.material.Stack
-import mui.material.StackDirection
 import mui.material.Step
 import mui.material.StepContent
 import mui.material.StepLabel
 import mui.material.Stepper
-import mui.material.TextField
-import mui.system.responsive
 import mui.system.sx
-import org.w3c.dom.HTMLInputElement
 import react.FC
 import react.Props
-import react.ReactNode
 import react.StateInstance
 import react.createContext
-import react.dom.onChange
-import react.useContext
 import react.useState
 import web.cssom.Padding
 import web.cssom.px
-import kotlin.time.Duration.Companion.milliseconds
 
 external interface CreateSavePageProps : Props
 
 val CreateSaveStepContext = createContext<StateInstance<Int>>()
 val NewSaveContext = createContext<StateInstance<Save>>()
+val NewSaveCreatingContext = createContext<StateInstance<Boolean>>()
 
 private val DEFAULT_SAVE = Save(
   name = SaveName(0),
@@ -47,75 +32,25 @@ private val DEFAULT_SAVE = Save(
 )
 
 val CreateSavePage = FC<CreateSavePageProps>("CreateSavePage") {
-  val appTheme by useContext(AppThemeContext)!!
-
-  val saveService = useContext(SaveServiceJs.Context)!!
-  val (_, savesListService) = useContext(SavesListService.Context)!!
-
   val stepState = useState(0)
   val step by stepState
 
   val newSaveState = useState(DEFAULT_SAVE)
-  var newSave by newSaveState
-
-  var creating by useState(false)
+  val creatingState = useState(false)
 
   CreateSaveStepContext(stepState) {
     NewSaveContext(newSaveState) {
-      Stepper {
-        sx {
-          padding = Padding(6.px, 12.px)
-        }
+      NewSaveCreatingContext(creatingState) {
+        Stepper {
+          sx { padding = Padding(6.px, 12.px) }
 
-        activeStep = step
-        orientation = Orientation.vertical
+          activeStep = step
+          orientation = Orientation.vertical
 
-        Step {
-          StepLabel { +"Name" }
-          StepContent {
-            Stack {
-              direction = responsive(StackDirection.column)
-
-              TextField {
-                sx { width = 384.px }
-
-                label = ReactNode("Save name")
-                required = true
-                fullWidth = false
-
-                value = newSave.displayName
-                onChange = { event ->
-                  val displayName = event.target.unsafeCast<HTMLInputElement>().value
-                  newSave = newSave.copy(displayName = displayName)
-                }
-              }
-
-              LoadingButton {
-                sx {
-                  width = 192.px
-                  marginTop = appTheme.spacing(3)
-                }
-
-                disabled = newSave.displayName.isBlank()
-                loading = creating
-                onClick = {
-                  launchMain {
-                    delay(3000.milliseconds)
-
-                    val request = CreateSaveRequest(
-                      save = newSave.copy(name = SaveName.createRandom())
-                    )
-                    val save = saveService.createSave(request)
-                    savesListService.ifLoaded { it.add(save) }
-
-                    creating = false
-                  }
-
-                  creating = true
-                }
-
-                +"Create Save"
-              }
+          Step {
+            StepLabel { +"Name" }
+            StepContent {
+              DisplayNameStep {}
             }
           }
         }
--- src/jsMain/kotlin/app/routes/createsave/DisplayNameStep.kt
+++ src/jsMain/kotlin/app/routes/createsave/DisplayNameStep.kt
@@ -1,10 +1,79 @@
 package app.routes.createsave
 
+import app.api.save.v1.CreateSaveRequest
+import app.api.save.v1.SaveName
+import app.api.save.v1.SaveServiceJs
+import app.data.save.SavesListService
+import app.theme.AppThemeContext
+import app.util.launchMain
+import kotlinx.coroutines.delay
+import mui.lab.LoadingButton
+import mui.material.Stack
+import mui.material.StackDirection
+import mui.material.TextField
+import mui.system.responsive
+import mui.system.sx
+import org.w3c.dom.HTMLInputElement
 import react.FC
 import react.Props
+import react.ReactNode
+import react.dom.onChange
+import react.useContext
+import web.cssom.px
+import kotlin.time.Duration.Companion.milliseconds
 
 external interface DisplayNameStepProps : Props
 
-val DisplayNameStep = FC<DisplayNameStepProps>("DisplayNameStep") { props ->
+val DisplayNameStep = FC<DisplayNameStepProps>("DisplayNameStep") {
+  val appTheme by useContext(AppThemeContext)!!
+  val saveService = useContext(SaveServiceJs.Context)!!
+  val (_, savesListService) = useContext(SavesListService.Context)!!
 
+  var newSave by useContext(NewSaveContext)!!
+  var creating by useContext(NewSaveCreatingContext)!!
+
+  Stack {
+    direction = responsive(StackDirection.column)
+
+    TextField {
+      sx { width = 384.px }
+
+      label = ReactNode("Save name")
+      required = true
+      disabled = creating
+
+      value = newSave.displayName
+      onChange = { event ->
+        val displayName = event.target.unsafeCast<HTMLInputElement>().value
+        newSave = newSave.copy(displayName = displayName)
+      }
+    }
+
+    LoadingButton {
+      sx {
+        width = 192.px
+        marginTop = appTheme.spacing(3)
+      }
+
+      disabled = newSave.displayName.isBlank()
+      loading = creating
+      onClick = {
+        launchMain {
+          delay(3000.milliseconds)
+
+          val request = CreateSaveRequest(
+            save = newSave.copy(name = SaveName.createRandom())
+          )
+          val save = saveService.createSave(request)
+          savesListService.ifLoaded { it.add(save) }
+
+          creating = false
+        }
+
+        creating = true
+      }
+
+      +"Create Save"
+    }
+  }
 }
