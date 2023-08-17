8e99c2dd0f6bded51c91088a7f2ab6547f088a74
chichumungaiii <chris@thesharpehouse.com>
2023-08-16 20:50:02 -0700

    Prepare the PlanPage for implementation.

--- src/jsMain/kotlin/app/routes/plan/PlanPage.kt
+++ src/jsMain/kotlin/app/routes/plan/PlanPage.kt
@@ -1,16 +1,23 @@
 package app.routes.plan
 
 import app.api.plan.v1.Plan
+import app.theme.AppThemeContext
 import mui.material.Box
+import mui.system.sx
 import react.FC
 import react.Props
+import react.useContext
 
 external interface PlanPageProps : Props {
   var plan: Plan
 }
 
 val PlanPage = FC<PlanPageProps>("PlanPage") { props ->
+  val (appTheme) = useContext(AppThemeContext)!!
+
   Box {
+    sx { padding = appTheme.spacing(2) }
+
     +props.plan.displayName
   }
 }
