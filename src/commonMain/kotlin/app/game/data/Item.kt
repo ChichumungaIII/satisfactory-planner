7bf56d0aafb6622ac3e3ef4de32956d8acde7470
chichumungaiii <chris@thesharpehouse.com>
2023-08-20 16:28:18 -0700

    Transcribe additional game data.

--- src/commonMain/kotlin/app/game/data/Item.kt
+++ src/commonMain/kotlin/app/game/data/Item.kt
@@ -1,15 +1,17 @@
 package app.game.data
 
 import app.game.logic.Condition
+import app.game.logic.Condition.Companion.any
 import app.game.logic.Condition.MilestoneCondition
 import app.game.logic.Condition.PhaseCondition
+import app.game.logic.Condition.ResearchCondition
 import util.math.Rational
 import util.math.q
 
 enum class Item(
   val displayName: String,
   val category: Category,
-  val stack: Rational,
+  val stack: Rational? = null,
   val sink: Rational? = null,
   val energy: Rational? = null,
   val radiation: Rational? = null,
@@ -30,21 +32,21 @@ enum class Item(
   ),
   IRON_INGOT(
     "Iron Ingot",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 100.q,
     sink = 2.q,
     unlock = PhaseCondition(Phase.GAME_START),
   ),
   IRON_PLATE(
     "Iron Plate",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 200.q,
     sink = 6.q,
     unlock = PhaseCondition(Phase.GAME_START),
   ),
   IRON_ROD(
     "Iron Rod",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 200.q,
     sink = 4.q,
     unlock = PhaseCondition(Phase.GAME_START),
@@ -78,21 +80,21 @@ enum class Item(
   ),
   COPPER_INGOT(
     "Copper Ingot",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 100.q,
     sink = 6.q,
     unlock = MilestoneCondition(Milestone.HUB_UPGRADE_2),
   ),
   WIRE(
     "Wire",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 500.q,
     sink = 6.q,
     unlock = MilestoneCondition(Milestone.HUB_UPGRADE_2),
   ),
   CABLE(
     "Cable",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 200.q,
     sink = 24.q,
     unlock = MilestoneCondition(Milestone.HUB_UPGRADE_2),
@@ -109,21 +111,21 @@ enum class Item(
   ),
   CONCRETE(
     "Concrete",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 500.q,
     sink = 12.q,
     unlock = MilestoneCondition(Milestone.HUB_UPGRADE_3),
   ),
   SCREW(
     "Screw",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 500.q,
     sink = 2.q,
     unlock = MilestoneCondition(Milestone.HUB_UPGRADE_3),
   ),
   REINFORCED_IRON_PLATE(
     "Reinforced Iron Plate",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 100.q,
     sink = 120.q,
     unlock = MilestoneCondition(Milestone.HUB_UPGRADE_3),
@@ -133,7 +135,7 @@ enum class Item(
 
   BIOMASS(
     "Biomass",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 200.q,
     sink = 12.q,
     energy = 180.q,
@@ -146,18 +148,18 @@ enum class Item(
 
   /* Field Research */
 
-  BEACON(
-    "Beacon",
-    Category.EQUIPMENT,
-    stack = 100.q,
-    sink = 320.q,
-    unlock = MilestoneCondition(Milestone.FIELD_RESEARCH),
-  ),
   OBJECT_SCANNER(
     "Object scanner",
     Category.EQUIPMENT,
     stack = 1.q,
     sink = 1_400.q,
+    unlock = MilestoneCondition(Milestone.FIELD_RESEARCH),
+  ),
+  BEACON(
+    "Beacon",
+    Category.EQUIPMENT,
+    stack = 100.q,
+    sink = 320.q,
     unlock = MilestoneCondition(Milestone.FIELD_RESEARCH),
   ),
 
@@ -169,28 +171,28 @@ enum class Item(
 
   COPPER_SHEET(
     "Copper Sheet",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 200.q,
     sink = 24.q,
     unlock = MilestoneCondition(Milestone.PART_ASSEMBLY),
   ),
   ROTOR(
     "Rotor",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 100.q,
     sink = 140.q,
     unlock = MilestoneCondition(Milestone.PART_ASSEMBLY),
   ),
   MODULAR_FRAME(
     "Modular Frame",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 50.q,
     sink = 408.q,
     unlock = MilestoneCondition(Milestone.PART_ASSEMBLY),
   ),
   SMART_PLATING(
     "Smart Plating",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 50.q,
     sink = 520.q,
     unlock = MilestoneCondition(Milestone.PART_ASSEMBLY),
@@ -198,14 +200,6 @@ enum class Item(
 
   /* Obstacle Clearing */
 
-  SOLID_BIOFUEL(
-    "Solid Biofuel",
-    Category.COMPONENTS,
-    stack = 200.q,
-    sink = 48.q,
-    energy = 450.q,
-    unlock = MilestoneCondition(Milestone.OBSTACLE_CLEARING),
-  ),
   CHAINSAW(
     "Chainsaw",
     Category.EQUIPMENT,
@@ -213,17 +207,448 @@ enum class Item(
     sink = 2760.q,
     unlock = MilestoneCondition(Milestone.OBSTACLE_CLEARING),
   ),
+  SOLID_BIOFUEL(
+    "Solid Biofuel",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 48.q,
+    energy = 450.q,
+    unlock = MilestoneCondition(Milestone.OBSTACLE_CLEARING),
+  ),
 
   /* Resource Sink Bonus Program */
 
   COLOR_CARTRIDGE(
     "Color Cartridge",
-    Category.COMPONENTS,
+    Category.PARTS,
     stack = 200.q,
     sink = 10.q,
     energy = 900.q,
     unlock = MilestoneCondition(Milestone.RESOURCE_SINK_BONUS_PROGRAM),
   ),
+
+  /* ****** */
+  /* TIER 3 */
+  /* ****** */
+
+  /* Coal Power */
+
+  COAL(
+    "Coal",
+    Category.RESOURCES,
+    stack = 100.q,
+    sink = 3.q,
+    energy = 300.q,
+    unlock = MilestoneCondition(Milestone.COAL_POWER),
+  ),
+
+  /* Basic Steel Production */
+
+  STEEL_INGOT(
+    "Steel Ingot",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 8.q,
+    unlock = MilestoneCondition(Milestone.BASIC_STEEL_PRODUCTION),
+  ),
+  STEEL_BEAM(
+    "Steel Beam",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 64.q,
+    unlock = MilestoneCondition(Milestone.BASIC_STEEL_PRODUCTION),
+  ),
+  STEEL_PIPE(
+    "Steel Pipe",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 24.q,
+    unlock = MilestoneCondition(Milestone.BASIC_STEEL_PRODUCTION),
+  ),
+
+  /* Improved Melee Combat */
+
+  XENO_BASHER(
+    "Xeno-Basher",
+    Category.EQUIPMENT,
+    stack = 1.q,
+    sink = 18_800.q,
+    unlock = MilestoneCondition(Milestone.IMPROVED_MELEE_COMBAT),
+  ),
+
+  /* ****** */
+  /* TIER 4 */
+  /* ****** */
+
+  /* Advanced Steel Production */
+
+  ENCASED_INDUSTRIAL_BEAM(
+    "Encased Industrial Beam",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 632.q,
+    unlock = MilestoneCondition(Milestone.ADVANCED_STEEL_PRODUCTION),
+  ),
+  STATOR(
+    "Stator",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 240.q,
+    unlock = MilestoneCondition(Milestone.ADVANCED_STEEL_PRODUCTION),
+  ),
+  MOTOR(
+    "Motor",
+    Category.PARTS,
+    stack = 50.q,
+    sink = 1_520.q,
+    unlock = MilestoneCondition(Milestone.ADVANCED_STEEL_PRODUCTION),
+  ),
+  AUTOMATED_WIRING(
+    "Automated Wiring",
+    Category.PARTS,
+    stack = 50.q,
+    sink = 1_440.q,
+    unlock = MilestoneCondition(Milestone.ADVANCED_STEEL_PRODUCTION),
+  ),
+  HEAVY_MODULAR_FRAME(
+    "Heavy Modular Frame",
+    Category.PARTS,
+    stack = 50.q,
+    sink = 11_520.q,
+    unlock = MilestoneCondition(Milestone.ADVANCED_STEEL_PRODUCTION),
+  ),
+
+  /* ****** */
+  /* TIER 5 */
+  /* ****** */
+
+  /* Oil Processing */
+
+  CRUDE_OIL(
+    "Crude Oil",
+    Category.RESOURCES,
+    energy = 320.q,
+    unlock = MilestoneCondition(Milestone.OIL_PROCESSING),
+  ),
+  PLASTIC(
+    "Plastic",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 75.q,
+    unlock = MilestoneCondition(Milestone.OIL_PROCESSING),
+  ),
+  HEAVY_OIL_RESIDUE(
+    "Heavy Oil Residue",
+    Category.PARTS,
+    energy = 400.q,
+    unlock = MilestoneCondition(Milestone.OIL_PROCESSING),
+  ),
+  RUBBER(
+    "Rubber",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 60.q,
+    unlock = MilestoneCondition(Milestone.OIL_PROCESSING)
+  ),
+  FUEL(
+    "Fuel",
+    Category.PARTS,
+    energy = 750.q,
+    unlock = MilestoneCondition(Milestone.OIL_PROCESSING),
+  ),
+  POLYMER_RESIN(
+    "Polymer Resin",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 12.q,
+    unlock = MilestoneCondition(Milestone.OIL_PROCESSING),
+  ),
+  PETROLEUM_COKE(
+    "Petroleum Coke",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 20.q,
+    energy = 180.q,
+    unlock = MilestoneCondition(Milestone.OIL_PROCESSING),
+  ),
+  CIRCUIT_BOARD(
+    "Circuit Board",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 696.q,
+    unlock = MilestoneCondition(Milestone.OIL_PROCESSING),
+  ),
+
+  /* Industrial Manufacturing */
+
+  COMPUTER(
+    "Computer",
+    Category.PARTS,
+    stack = 50.q,
+    sink = 17_260.q,
+    unlock = MilestoneCondition(Milestone.INDUSTRIAL_MANUFACTURING),
+  ),
+  MODULAR_ENGINE(
+    "Modular Engine",
+    Category.PARTS,
+    stack = 50.q,
+    sink = 9_960.q,
+    unlock = MilestoneCondition(Milestone.INDUSTRIAL_MANUFACTURING),
+  ),
+  ADAPTIVE_CONTROL_UNIT(
+    "Adaptive Control Unit",
+    Category.PARTS,
+    stack = 50.q,
+    sink = 86_120.q,
+    unlock = MilestoneCondition(Milestone.INDUSTRIAL_MANUFACTURING),
+  ),
+
+  /* Alternative Fluid Transport */
+
+  EMPTY_CANISTER(
+    "Empty Canister",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 60.q,
+    unlock = MilestoneCondition(Milestone.ALTERNATIVE_FLUID_TRANSPORT),
+  ),
+  PACKAGED_WATER(
+    "Packaged Water",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 130.q,
+    unlock = MilestoneCondition(Milestone.ALTERNATIVE_FLUID_TRANSPORT),
+  ),
+  PACKAGED_OIL(
+    "Packaged Oil",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 180.q,
+    energy = 320.q,
+    unlock = MilestoneCondition(Milestone.ALTERNATIVE_FLUID_TRANSPORT),
+  ),
+  PACKAGED_FUEL(
+    "Packaged Fuel",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 270.q,
+    energy = 750.q,
+    unlock = MilestoneCondition(Milestone.ALTERNATIVE_FLUID_TRANSPORT),
+  ),
+  PACKAGED_HEAVY_OIL_RESIDUE(
+    "Packaged Heavy Oil Residue",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 180.q,
+    energy = 400.q,
+    unlock = MilestoneCondition(Milestone.ALTERNATIVE_FLUID_TRANSPORT),
+  ),
+  PACKAGED_LIQUID_BIOFUEL(
+    "Packaged Liquid Biofuel",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 370.q,
+    energy = 750.q,
+    unlock = MilestoneCondition(Milestone.ALTERNATIVE_FLUID_TRANSPORT),
+  ),
+  LIQUID_BIOFUEL(
+    "Liquid Biofuel",
+    Category.PARTS,
+    energy = 750.q,
+    unlock = MilestoneCondition(Milestone.ALTERNATIVE_FLUID_TRANSPORT),
+  ),
+
+  /* Gas Mask */
+
+  GAS_MASK(
+    "Gas Mask",
+    Category.EQUIPMENT,
+    stack = 1.q,
+    sink = 55_000.q,
+    unlock = MilestoneCondition(Milestone.GAS_MASK),
+  ),
+  GAS_FILTER(
+    "Gas Filter",
+    Category.PARTS,
+    stack = 50.q,
+    sink = 830.q,
+    unlock = MilestoneCondition(Milestone.GAS_MASK),
+  ),
+
+  /* ****** */
+  /* TIER 6 */
+  /* ****** */
+
+  /* Expanded Power Infrastructure */
+
+  CATERIUM_ORE(
+    "Caterium Ore",
+    Category.RESOURCES,
+    stack = 100.q,
+    sink = 7.q,
+    unlock = any {
+      +Milestone.EXPANDED_POWER_INFRASTRUCTURE
+      +Research.UNKNOWN_METAL
+    }
+  ),
+  CATERIUM_INGOT(
+    "Caterium Ingot",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 42.q,
+    unlock = ResearchCondition(Research.CATERIUM_INGOT),
+  ),
+  QUICKWIRE(
+    "Quickwire",
+    Category.PARTS,
+    stack = 500.q,
+    sink = 17.q,
+    unlock = ResearchCondition(Research.QUICKWIRE),
+  ),
+  ZIPLINE(
+    "Zipline",
+    Category.EQUIPMENT,
+    stack = 1.q,
+    sink = 5_284.q,
+    unlock = ResearchCondition(Research.ZIPLINE),
+  ),
+  AI_LIMITER(
+    "AI Limiter",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 920.q,
+    unlock = ResearchCondition(Research.AI_LIMITER),
+  ),
+  HIGH_SPEED_CONNECTOR(
+    "High-Speed Connector",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 3_776.q,
+    unlock = ResearchCondition(Research.HIGH_SPEED_CONNECTOR),
+  ),
+
+  /* Jetpack */
+
+  JETPACK(
+    "Jetpack",
+    Category.EQUIPMENT,
+    stack = 1.q,
+    sink = 49_580.q,
+    unlock = MilestoneCondition(Milestone.JETPACK),
+  ),
+
+  /* ****** */
+  /* TIER 7 */
+  /* ****** */
+
+  /* Bauxite Refinement */
+
+  BAUXITE(
+    "Bauxite",
+    Category.RESOURCES,
+    stack = 100.q,
+    sink = 8.q,
+    unlock = MilestoneCondition(Milestone.BAUXITE_REFINEMENT),
+  ),
+  ALUMINA_SOLUTION(
+    "Alumina Solution",
+    Category.PARTS,
+    unlock = MilestoneCondition(Milestone.BAUXITE_REFINEMENT),
+  ),
+  PACKAGED_ALUMINA_SOLUTION(
+    "Packaged Alumina Solution",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 160.q,
+    unlock = MilestoneCondition(Milestone.BAUXITE_REFINEMENT),
+  ),
+  ALUMINUM_SCRAP(
+    "Aluminum Scrap",
+    Category.PARTS,
+    stack = 500.q,
+    sink = 27.q,
+    unlock = MilestoneCondition(Milestone.BAUXITE_REFINEMENT),
+  ),
+  ALUMINUM_INGOT(
+    "Aluminum Ingot",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 131.q,
+    unlock = MilestoneCondition(Milestone.BAUXITE_REFINEMENT),
+  ),
+  ALCLAD_ALUMINUM_SHEET(
+    "Alclad Aluminum Sheet",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 266.q,
+    unlock = MilestoneCondition(Milestone.BAUXITE_REFINEMENT),
+  ),
+  ALUMINUM_CASING(
+    "Aluminum Casing",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 393.q,
+    unlock = MilestoneCondition(Milestone.BAUXITE_REFINEMENT),
+  ),
+  RAW_QUARTZ(
+    "Raw Quartz",
+    Category.RESOURCES,
+    stack = 100.q,
+    sink = 15.q,
+    unlock = any {
+      +Milestone.BAUXITE_REFINEMENT
+      +Research.UNKNOWN_CRYSTALLINE_MATERIAL
+    },
+  ),
+  QUARTZ_CRYSTAL(
+    "Quartz Crystal",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 50.q,
+    unlock = ResearchCondition(Research.QUARTZ_CRYSTALS),
+  ),
+  SILICA(
+    "Silica",
+    Category.PARTS,
+    stack = 200.q,
+    sink = 20.q,
+    unlock = ResearchCondition(Research.SILICA),
+  ),
+  CRYSTAL_OSCILATOR(
+    "Crystal Oscilator",
+    Category.PARTS,
+    stack = 100.q,
+    sink = 3_702.q,
+    unlock = ResearchCondition(Research.CRYSTAL_OSCILLATOR),
+  ),
+  RADIO_CONTROL_UNIT(
+    "Radio Control Unit",
+    Category.PARTS,
+    stack = 50.q,
+    sink = 19_600.q,
+    unlock = MilestoneCondition(Milestone.BAUXITE_REFINEMENT),
+  ),
+
+  /* Hazmat Suit */
+
+  HAZMAT_SUIT(
+    "Hazmat Suit",
+    Category.EQUIPMENT,
+    stack = 1.q,
+    sink = 54_100.q,
+    unlock = MilestoneCondition(Milestone.HAZMAT_SUIT),
+  ),
+  IODINE_INFUSED_FILTER(
+    "Iodine Infused Filter",
+    Category.PARTS,
+    stack = 50.q,
+    sink = 2_718.q,
+    unlock = MilestoneCondition(Milestone.HAZMAT_SUIT),
+  ),
+
+  /* Aeronautical Engineering */
+
+  // TODO: Sulfur
 
   /* ****** */
   /* NATURE */
@@ -332,7 +757,7 @@ enum class Item(
   enum class Category(
     val displayName: String,
   ) {
-    COMPONENTS("Components"),
+    PARTS("Components"),
     EQUIPMENT("Equipment"),
     NATURE("Nature"),
     RESOURCES("Resources"),
--- src/commonMain/kotlin/app/game/data/Research.kt
+++ src/commonMain/kotlin/app/game/data/Research.kt
@@ -2,6 +2,7 @@ package app.game.data
 
 import app.game.logic.Condition
 import app.game.logic.Condition.Companion.all
+import app.game.logic.Condition.ResearchCondition
 import util.math.Rational
 import util.math.q
 
@@ -78,6 +79,78 @@ enum class Research(
 
   /* Caterium */
 
+  UNKNOWN_METAL(
+    "Unknown Metal",
+    Category.CATERIUM,
+    cost = mapOf(),
+    requirement = Condition.TRUE,
+  ),
+  CATERIUM(
+    "Caterium",
+    Category.CATERIUM,
+    cost = mapOf(Item.CATERIUM_ORE to 10.q),
+    requirement = ResearchCondition(UNKNOWN_METAL),
+  ),
+  CATERIUM_INGOT(
+    "Caterium Ingot",
+    Category.CATERIUM,
+    cost = mapOf(Item.CATERIUM_ORE to 50.q),
+    requirement = ResearchCondition(CATERIUM),
+  ),
+  QUICKWIRE(
+    "Quickwire",
+    Category.CATERIUM,
+    cost = mapOf(Item.CATERIUM_INGOT to 50.q),
+    requirement = ResearchCondition(CATERIUM_INGOT),
+  ),
+  ZIPLINE(
+    "Zipline",
+    Category.CATERIUM,
+    cost = mapOf(
+      Item.QUICKWIRE to 100.q,
+      Item.CABLE to 50.q,
+    ),
+    requirement = ResearchCondition(QUICKWIRE),
+  ),
+  CATERIUM_ELECTRONICS(
+    "Caterium Electronics",
+    Category.CATERIUM,
+    cost = mapOf(Item.QUICKWIRE to 100.q),
+    requirement = ResearchCondition(QUICKWIRE),
+  ),
+  STUN_REBAR(
+    "Stun Rebar",
+    Category.CATERIUM,
+    cost = mapOf(
+      Item.QUICKWIRE to 50.q,
+      // TODO: Item.IRON_REBAR to 10.q,
+    ),
+    requirement = ResearchCondition(QUICKWIRE),
+  ),
+  AI_LIMITER(
+    "AI Limiter",
+    Category.CATERIUM,
+    cost = mapOf(
+      Item.QUICKWIRE to 200.q,
+      Item.COPPER_SHEET to 50.q,
+    ),
+    requirement = ResearchCondition(CATERIUM_ELECTRONICS),
+  ),
+  POWER_POLES_MK_2(
+    "Power Poles Mk. 2",
+    Category.CATERIUM,
+    cost = mapOf(Item.QUICKWIRE to 300.q),
+    requirement = ResearchCondition(CATERIUM_ELECTRONICS),
+  ),
+  HIGH_SPEED_CONNECTOR(
+    "High-Speed Connector",
+    Category.CATERIUM,
+    cost = mapOf(
+      Item.QUICKWIRE to 500.q,
+      Item.PLASTIC to 50.q,
+    ),
+    requirement = ResearchCondition(CATERIUM_ELECTRONICS),
+  ),
 
   /* Mycelia */
 
@@ -96,6 +169,48 @@ enum class Research(
 
   /* Quartz */
 
+  UNKNOWN_CRYSTALLINE_MATERIAL(
+    "Unknown Crystalline Material",
+    Category.QUARTZ,
+    cost = mapOf(),
+    requirement = Condition.TRUE,
+  ),
+  QUARTZ(
+    "Quartz",
+    Category.QUARTZ,
+    cost = mapOf(Item.RAW_QUARTZ to 10.q),
+    requirement = ResearchCondition(UNKNOWN_CRYSTALLINE_MATERIAL),
+  ),
+  QUARTZ_CRYSTALS(
+    "Quartz Crystals",
+    Category.QUARTZ,
+    cost = mapOf(Item.RAW_QUARTZ to 20.q),
+    requirement = ResearchCondition(QUARTZ),
+  ),
+  SILICA(
+    "Silica",
+    Category.QUARTZ,
+    cost = mapOf(Item.RAW_QUARTZ to 20.q),
+    requirement = ResearchCondition(QUARTZ),
+  ),
+  SHATTER_REBAR(
+    "Shatter Rebar",
+    Category.QUARTZ,
+    cost = mapOf(
+      Item.QUARTZ_CRYSTAL to 30.q,
+      // TODO: Item.IRON_REBAR to 150.q,
+    ),
+    requirement = ResearchCondition(QUARTZ_CRYSTALS),
+  ),
+  CRYSTAL_OSCILLATOR(
+    "Crystal Oscillator",
+    Category.QUARTZ,
+    cost = mapOf(
+      Item.QUARTZ_CRYSTAL to 100.q,
+      Item.REINFORCED_IRON_PLATE to 50.q,
+    ),
+    requirement = ResearchCondition(QUARTZ_CRYSTALS),
+  ),
 
   /* Sulfur */
 
