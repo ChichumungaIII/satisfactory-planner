3d9ceda59a55958f5cc8308dec1328faa36b84bb
chichumungaiii <chris@thesharpehouse.com>
2023-08-25 12:22:59 -0700

    Replace ItemAutocomplete with an AppAutocomplete.

--- src/jsMain/kotlin/app/common/input/ItemAutocomplete.kt
+++ src/jsMain/kotlin/app/common/input/ItemAutocomplete.kt
@@ -1,19 +1,7 @@
 package app.common.input
 
+import app.common.input.autocomplete.createAppAutocomplete
 import app.game.data.Item
-import app.util.PropsDelegate
-import js.core.jso
-import mui.material.Autocomplete
-import mui.material.AutocompleteProps
-import mui.material.ListProps
-import mui.material.ListSubheader
-import mui.material.TextField
-import mui.system.sx
-import react.FC
-import react.Props
-import react.ReactNode
-import react.create
-import web.cssom.Padding
 import web.cssom.px
 
 private val CATEGORY_ORDER = listOf(
@@ -27,47 +15,11 @@ private val ITEM_COMPARATOR =
     CATEGORY_ORDER.indexOf(it.category)
   }.thenBy { it.ordinal }
 
-external interface ItemAutocompleteProps : Props {
-  var model: Item
-  var setModel: (Item) -> Unit
-
-  var options: List<Item>
-}
-
-val ItemAutocomplete = FC<ItemAutocompleteProps>("ItemAutocomplete") { props ->
-  var model by PropsDelegate(props.model, props.setModel)
-
-  @Suppress("UPPER_BOUND_VIOLATED")
-  Autocomplete<AutocompleteProps<Item>> {
-    sx { width = 256.px }
-
-    renderInput = { params ->
-      TextField.create {
-        +params
-        label = ReactNode("Item")
-      }
-    }
-    ListboxComponent = mui.material.List
-    ListboxProps = jso<ListProps> {
-      sx { padding = Padding(0.px, 0.px) }
-    }
-
-    groupBy = { it.category.displayName }
-    renderGroup = { params ->
-      mui.material.List.create {
-        subheader = ListSubheader.create { +params.group }
-        +params.children
-      }
-    }
-
-    options = props.options.sortedWith(ITEM_COMPARATOR).toTypedArray()
-    getOptionLabel = { it.displayName }
-
-    value = model
-    onChange = { _, next: Item?, _, _ -> next?.also { model = it } }
-
-    autoComplete = true
-    autoHighlight = true
-    disableClearable = true
-  }
-}
+val ItemAutocomplete = createAppAutocomplete(
+  displayName = "ItemAutocomplete",
+  label = "Item",
+  grouping = { it.category.displayName },
+  render = Item::displayName,
+  ordering = ITEM_COMPARATOR,
+  width = 238.px,
+)
---
+++ src/jsMain/kotlin/app/common/input/autocomplete/AppAutocomplete.kt
@@ -0,0 +1,67 @@
+package app.common.input.autocomplete
+
+import app.util.PropsDelegate
+import js.core.jso
+import mui.material.Autocomplete
+import mui.material.AutocompleteProps
+import mui.material.ListProps
+import mui.material.ListSubheader
+import mui.material.TextField
+import mui.system.sx
+import react.FC
+import react.Props
+import react.ReactNode
+import react.create
+import web.cssom.Padding
+import web.cssom.Width
+import web.cssom.px
+
+external interface AppAutocompleteProps<T> : Props {
+  var model: T
+  var setModel: (T) -> Unit
+
+  var options: List<T>
+}
+
+fun <T> createAppAutocomplete(
+  displayName: String,
+  label: String,
+  grouping: (T) -> String,
+  render: (T) -> String,
+  ordering: Comparator<T>,
+  width: Width,
+) = FC<AppAutocompleteProps<T>>(displayName) { props ->
+  var model by PropsDelegate(props.model, props.setModel)
+
+  @Suppress("UPPER_BOUND_VIOLATED")
+  Autocomplete<AutocompleteProps<T>> {
+    sx { this.width = width }
+
+    renderInput = { params ->
+      TextField.create {
+        +params
+        this.label = ReactNode(label)
+      }
+    }
+    ListboxComponent = mui.material.List
+    ListboxProps = jso<ListProps> { sx { padding = Padding(0.px, 0.px) } }
+
+    groupBy = grouping
+    renderGroup = { params ->
+      mui.material.List.create {
+        subheader = ListSubheader.create { +params.group }
+        +params.children
+      }
+    }
+
+    options = props.options.sortedWith(ordering).toTypedArray()
+    getOptionLabel = render
+
+    value = model
+    onChange = { _, next: T?, _, _ -> next?.also { model = it } }
+
+    autoComplete = true
+    autoHighlight = true
+    disableClearable = true
+  }
+}
