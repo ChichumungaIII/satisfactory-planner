4bcabaab84ae694263a87a19688c5d7a735fd75e
chichumungaiii <chris@thesharpehouse.com>
2023-10-16 11:14:44 -0700

    Add more Optimize test cases for basic functionality.

--- src/jvmTest/kotlin/app/routes/optimize/v2/OptimizeRouteTest.kt
+++ src/jvmTest/kotlin/app/routes/optimize/v2/OptimizeRouteTest.kt
@@ -52,4 +52,78 @@ class OptimizeRouteTest {
     }
     assertEquals(expected, response)
   }
+
+  @Test
+  fun optimize_singleInput_balancedOutput() = runBlocking {
+    val request = optimizeRequest {
+      +input(Item.IRON_ORE, 120.q)
+      +productWeight(Item.IRON_PLATE, 1.q)
+      +productWeight(Item.IRON_ROD, 1.q)
+    }
+    val response = optimize(request)
+
+    val expected = optimizeResponse {
+      +input(Item.IRON_ORE, 120.q) {
+        consumption = 120.q
+        demand = 120.q
+      }
+      +product(Item.IRON_PLATE, 48.q, 48.q)
+      +product(Item.IRON_ROD, 48.q, 48.q)
+
+      Recipe.IRON_INGOT += 400.q / 100.q
+      Recipe.IRON_PLATE += 240.q / 100.q
+      Recipe.IRON_ROD += 320.q / 100.q
+    }
+    assertEquals(expected, response)
+  }
+
+  @Test
+  fun optimize_singleInput_imbalancedOutput() = runBlocking {
+    val request = optimizeRequest {
+      +input(Item.IRON_ORE, 120.q)
+      +productWeight(Item.IRON_PLATE, 2.q)
+      +productWeight(Item.IRON_ROD, 1.q)
+    }
+    val response = optimize(request)
+
+    val expected = optimizeResponse {
+      +input(Item.IRON_ORE, 120.q) {
+        consumption = 120.q
+        demand = 120.q
+      }
+      +product(Item.IRON_PLATE, 60.q, 60.q)
+      +product(Item.IRON_ROD, 30.q, 30.q)
+
+      Recipe.IRON_INGOT += 400.q / 100.q
+      Recipe.IRON_PLATE += 300.q / 100.q
+      Recipe.IRON_ROD += 200.q / 100.q
+    }
+    assertEquals(expected, response)
+  }
+
+  @Test
+  fun optimize_singleInput_offsetWeightedOutput() = runBlocking {
+    val request = optimizeRequest {
+      +input(Item.IRON_ORE, 120.q)
+      +productAmount(Item.IRON_PLATE, 20.q)
+      +productWeight(Item.IRON_PLATE, 1.q)
+      +productWeight(Item.IRON_ROD, 1.q)
+    }
+    val response = optimize(request)
+
+    val expected = optimizeResponse {
+      +input(Item.IRON_ORE, 120.q) {
+        consumption = 120.q
+        demand = 120.q
+      }
+      +product(Item.IRON_PLATE, 20.q, 56.q) // TODO: Fix "potential" calculation.
+      +product(Item.IRON_PLATE, 36.q, 36.q)
+      +product(Item.IRON_ROD, 36.q, 36.q)
+
+      Recipe.IRON_INGOT += 400.q / 100.q
+      Recipe.IRON_PLATE += 280.q / 100.q
+      Recipe.IRON_ROD += 240.q / 100.q
+    }
+    assertEquals(expected, response)
+  }
 }
