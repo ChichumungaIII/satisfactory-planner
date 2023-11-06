ba6970ae180b3254c48513ead5d97e9d8e054c4a
chichumungaiii <chris@thesharpehouse.com>
2023-11-06 15:09:34 -0800

    Adds the core structure and phase selector to EditSavePage.

--- src/jsMain/kotlin/app/redux/state/resource/save/useSave.kt
+++ src/jsMain/kotlin/app/redux/state/resource/save/useSave.kt
@@ -3,12 +3,15 @@ package app.redux.state.resource.save
 import app.api.save.v1.GetSaveRequest
 import app.api.save.v1.Save
 import app.api.save.v1.SaveName
+import app.api.save.v1.UpdateSaveRequest
 import app.api.save.v1.getSaveService
 import app.redux.state.resource.LoadResource
 import app.redux.state.resource.RegisterResource
 import app.redux.state.resource.RegisterResourceRequest
+import app.redux.state.resource.SaveResource
 import app.redux.state.resource.useResource
 import kotlinx.coroutines.Job
+import kotlin.time.Duration.Companion.seconds
 
 class LoadSave(name: SaveName) :
   LoadResource<SaveName, Save>(
@@ -18,6 +21,15 @@ class LoadSave(name: SaveName) :
     { save -> RegisterSave(save) },
     { request -> RegisterSaveRequest(name, request) }
   )
+
+class SaveSave(save: Save) :
+  SaveResource<SaveName, Save>(
+    save,
+    { state -> state.saveCache },
+    3.seconds,
+    { getSaveService().updateSave(UpdateSaveRequest(save, listOf())) },
+    { RegisterSave(it) },
+    { request -> RegisterSaveRequest(save.name, request) })
 
 private class RegisterSave(save: Save) :
   SaveCacheAction(RegisterResource(Unit, save))
