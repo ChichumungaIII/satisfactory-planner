9b0640307672353a14680aef060749971d0effa1
Chris Sharpe <chris@thesharpehouse.com>
2025-07-11 13:37:49 -0700

    Adds additional IRON_INGOT recipes.

--- src/commonMain/kotlin/app/game/data/RecipeV2.kt
+++ src/commonMain/kotlin/app/game/data/RecipeV2.kt
@@ -32,6 +32,50 @@ enum class RecipeV2(
     ),
     product = 1 of Item.IRON_INGOT,
   ),
+  IRON_ALLOT_INGOT(
+    "Iron Allot Ingot",
+    category = Category.INGOTS,
+    time = 12,
+    inputs = listOf(
+      8 of Item.IRON_ORE,
+      2 of Item.COPPER_ORE,
+    ),
+    product = 15 of Item.IRON_INGOT,
+    alternate = true,
+  ),
+  BASIC_IRON_INGOT(
+    "Basic Iron Ingot",
+    category = Category.INGOTS,
+    time = 12,
+    inputs = listOf(
+      5 of Item.IRON_ORE,
+      8 of Item.LIMESTONE,
+    ),
+    product = 10 of Item.IRON_INGOT,
+    alternate = true,
+  ),
+  PURE_IRON_INGOT(
+    "Pure Iron Ingot",
+    category = Category.INGOTS,
+    time = 12,
+    inputs = listOf(
+      7 of Item.IRON_ORE,
+      4 of Item.WATER,
+    ),
+    product = 13 of Item.IRON_INGOT,
+    alternate = true,
+  ),
+  LEACHED_IRON_INGOT(
+    "Leached Iron Ingot",
+    category = Category.INGOTS,
+    time = 6,
+    inputs = listOf(
+      5 of Item.IRON_ORE,
+      1 of Item.SULFURIC_ACID,
+    ),
+    product = 10 of Item.IRON_INGOT,
+    alternate = true,
+  ),
 
   ;
 
